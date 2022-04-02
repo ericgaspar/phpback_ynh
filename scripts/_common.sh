677d79d3b3cdafaf356d2ea0f1d69077c7541b03
ericgaspar <junk.eg@free.fr>
2022-04-02 14:08:15 +0200

    Fix

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,8 +4,9 @@
 # COMMON VARIABLES
 #=================================================
 
-# dependencies used by the app
-pkg_dependencies="deb1 deb2 php$YNH_DEFAULT_PHP_VERSION-deb1 php$YNH_DEFAULT_PHP_VERSION-deb2"
+YNH_PHP_VERSION="7.4"
+
+pkg_dependencies="php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-cli php${YNH_PHP_VERSION}-mysql php${YNH_PHP_VERSION}-xml php${YNH_PHP_VERSION}-mbstring"
 
 #=================================================
 # PERSONAL HELPERS
